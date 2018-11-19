d6c57a5c806d618169363f47673153ca6e68af73
shevchVit <45031357+shevchVit@users.noreply.github.com>
2018-11-19 18:47:14 +1000

    Add files via upload

---
+++ BrushlessMotor.cpp
@@ -0,0 +1,20 @@
+#include "Motors.h"
+
+BrushlessMotor::BrushlessMotor(uint8_t pin)
+{
+	m_pin = pin;
+}
+
+void BrushlessMotor::init()
+{
+	m_driver.attach(m_pin);
+	m_driver.writeMicroseconds(1500);
+}
+
+void BrushlessMotor::set_power(int8_t power)
+{
+	m_power = constrain(power, -100, 100);
+	if (m_is_inverse) m_power *= -1;
+	m_power = map(m_power, -100, 100, 1000, 2000);
+	m_driver.writeMicroseconds(m_power);
+}
